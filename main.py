6b587d2f32dd532112bdc23a112b3604fa159b7f
KIPER <gratviyt@gmail.com>
2023-12-08 19:16:12 +0300

    21

--- main.py
+++ main.py
@@ -1,25 +1,23 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QGraphicsScene, QGraphicsView, QGraphicsEllipseItem
+from PyQt5.QtWidgets import QApplication, QMainWindow, QGraphicsScene, QGraphicsEllipseItem
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QColor
-from UI import Ui_MainWindow
+from PyQt5 import uic
 import random
 
-class MyMainWindow(QMainWindow, Ui_MainWindow):
+class MyMainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setupUi(self)
+        uic.loadUi("UI.ui", self)
         self.pushButton.clicked.connect(self.create_circle)
 
     def create_circle(self):
         scene = QGraphicsScene()
-        view = QGraphicsView(scene)
-        view.setGeometry(100, 100, 400, 400)
         diameter = random.randint(50, 150)
         ellipse = QGraphicsEllipseItem(0, 0, diameter, diameter)
         ellipse.setBrush(QColor(Qt.yellow))
         scene.addItem(ellipse)
-        view.show()
+        self.graphicsView.setScene(scene)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
